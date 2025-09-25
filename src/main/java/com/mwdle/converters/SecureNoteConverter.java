5196259e83689cf8fb9885049f123b71eea5acad
mwdle <mwdle@mwdle.com>
2025-09-25 00:27:26 -0600

    Adjusted secure note converter to convert to a file if the item name ends with .env. This feature is intended to better support Docker Compose in pipelines.

--- src/main/java/com/mwdle/converters/SecureNoteConverter.java
+++ src/main/java/com/mwdle/converters/SecureNoteConverter.java
@@ -1,14 +1,20 @@
 package com.mwdle.converters;
 
 import com.cloudbees.plugins.credentials.CredentialsScope;
+import com.cloudbees.plugins.credentials.SecretBytes;
+import com.cloudbees.plugins.credentials.common.StandardCredentials;
 import com.mwdle.model.BitwardenItem;
 import hudson.Extension;
 import hudson.util.Secret;
-import org.jenkinsci.plugins.plaincredentials.StringCredentials;
+import org.jenkinsci.plugins.plaincredentials.impl.FileCredentialsImpl;
 import org.jenkinsci.plugins.plaincredentials.impl.StringCredentialsImpl;
 
+import java.util.UUID;
+
 /**
- * Converts a Bitwarden 'Secure Note' item into a Jenkins {@link StringCredentials}.
+ * Converts a Bitwarden 'Secure Note' item into a Jenkins credential.
+ * If the name ends with ".env", treat it as a {@link FileCredentialsImpl}.
+ * Otherwise, default to {@link StringCredentialsImpl}.
  */
 @Extension
 public class SecureNoteConverter extends BitwardenItemConverter {
@@ -25,10 +31,15 @@ public class SecureNoteConverter extends BitwardenItemConverter {
     /**
      * {@inheritDoc}
      * <p>
-     * Constructs a {@link StringCredentialsImpl} using the content of the {@code notes} field.
+     * Returns a {@link StringCredentialsImpl} using the content of the {@code notes} field.
+     * Returns a {@link FileCredentialsImpl} instead if the item name ends with .env (intended to support Docker Compose)
      */
     @Override
-    public StringCredentials convert(CredentialsScope scope, String id, String description, BitwardenItem item) {
-        return new StringCredentialsImpl(scope, id, description, Secret.fromString(item.getNotes()));
+    public StandardCredentials convert(CredentialsScope scope, String id, String description, BitwardenItem item) {
+        if (item.getName().toLowerCase().endsWith(".env")) {
+            return new FileCredentialsImpl(scope, id, description, UUID.randomUUID().toString(), SecretBytes.fromString(item.getNotes()));
+        } else {
+            return new StringCredentialsImpl(scope, id, description, Secret.fromString(item.getNotes()));
+        }
     }
 }
