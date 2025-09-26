c79efd009e77f4394ca541cba67f8eb37de70f31
mwdle <mwdle@mwdle.com>
2025-09-25 23:55:52 -0600

    Fixed spotbugs suppressions.

--- src/main/java/com/mwdle/model/BitwardenItem.java
+++ src/main/java/com/mwdle/model/BitwardenItem.java
@@ -2,6 +2,7 @@ package com.mwdle.model;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
+import edu.umd.cs.findbugs.annotations.SuppressFBWarnings;
 import hudson.util.Secret;
 
 /**
@@ -13,6 +14,8 @@ import hudson.util.Secret;
 // This Jackson annotation ensures that if the Bitwarden CLI adds new, unknown fields
 // to its JSON output in the future, the deserialization process will not fail.
 @JsonIgnoreProperties(ignoreUnknown = true)
+// Suppress SpotBugs warning for fields populated by the Jackson JSON parser
+@SuppressFBWarnings("UWF_UNWRITTEN_FIELD")
 public class BitwardenItem {
     /**
      * The unique UUID of the item.
--- src/main/java/com/mwdle/model/BitwardenLogin.java
+++ src/main/java/com/mwdle/model/BitwardenLogin.java
@@ -2,15 +2,12 @@ package com.mwdle.model;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
-import edu.umd.cs.findbugs.annotations.SuppressFBWarnings;
 import hudson.util.Secret;
 
 /**
  * Represents the nested 'login' object within a Bitwarden item JSON.
  */
 @JsonIgnoreProperties(ignoreUnknown = true)
-// Suppress SpotBugs warning for fields populated by the Jackson JSON parser
-@SuppressFBWarnings("UWF_UNWRITTEN_FIELD")
 public class BitwardenLogin {
     /**
      * The username associated with the login.
